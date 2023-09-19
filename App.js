e9b9596336f3c6d7fa0490cf40a2977eb03266a2
NaumanRecat <naumantariq2977@gmail.com>
2023-09-19 20:14:27 +0500

    Redux Initial

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from 'react-native';
+import Header from "./component/Header";
+import Product from "./component/Product";
+
+const App =()=>{
+  return(
+    <Product/>
+    // <Header/>
+  )
+}
+export default App
